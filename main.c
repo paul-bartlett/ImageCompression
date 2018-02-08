a571346b34733d2a395ca629811547daa7de14cd
paul-bartlett <pbartle7@uwo.ca>
2018-02-08 16:20:38 -0500

    testing done, program should be ready for submission

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])  {
     struct PPM_Image pic_ppm;
 
     if(argc != 6) {
-        printf("You must supply 5 arguments: image type code (1 for pbm, 2 for pgm, or 3 for ppm), image width, image height, output image name, and image format (0 for ASCII, 1 raw)\n");
+        printf("You must supply 5 arguments: image type code (1 for pbm, 2 for pgm, or 3 for ppm), image width, image height, output image name, and image format (0 for ASCII, 1 for raw)\n");
         return 0;
     }
 
@@ -47,6 +47,11 @@ int main(int argc, char *argv[])  {
         printf("Please enter a valid image type (1-3)\n");
         return 0;
     }   
+    // Check for valid image format
+    if(image_format != 0 && image_format != 1) {
+        printf("Please enter a valid image format (0 for ASCII, 1 for raw)\n");
+        return 0;
+    }
 
     // Decide which of the 3 image types to create (PBM, PGM, PPM)
     switch(image_type) {
@@ -58,6 +63,7 @@ int main(int argc, char *argv[])  {
             // Iterate through all pixels and set pixel white if in the middle, else set to black
             for(row = 0; row<pic_pbm.height; row++) {
                 for(col = 0; col<pic_pbm.width; col++) {
+                    // Change pixel to white in the middle, else black
                     if(row >= pic_pbm.height/4 && row < (pic_pbm.height*3)/4 && col >= pic_pbm.width/4 && col < (pic_pbm.width*3)/4)
                         pic_pbm.image[row][col] = 0; // white
                     else
@@ -74,7 +80,7 @@ int main(int argc, char *argv[])  {
                     pic_pbm.image[row][(int)(row/slope)] = 1; // black
                     pic_pbm.image[row][pic_pbm.width - 1 - (int)(row/slope)] = 1; 
                 }
-                // Column by column to cover cases when width is greater (or square images)
+            // Column by column to cover cases when width is greater (or square images)
             } else { 
                 for(col = 0; col<pic_pbm.width; col++) {
                     pic_pbm.image[(int)(col*slope)][col] = 1; // black
@@ -94,6 +100,7 @@ int main(int argc, char *argv[])  {
             // Iterate through all pixels and set pixel white if in the middle, else set to black
             for(row = 0; row<pic_pgm.height; row++) {
                 for(col = 0; col<pic_pgm.width; col++) {
+                    // Change pixel to white in the middle, else black
                     if(row >= pic_pgm.height/4 && row < (pic_pgm.height*3)/4 && col >= pic_pgm.width/4 && col < (pic_pgm.width*3)/4)
                         pic_pgm.image[row][col] = 255; // white
                     else
@@ -103,7 +110,8 @@ int main(int argc, char *argv[])  {
 
             // Calculate slope for the increase in darkness towards the bottom
             slope = (float)pic_pgm.height / (float)pic_pgm.width;
-
+            
+            // Iterate by column if height is greater for proper proportional grey colour, else iterate by row
             if(pic_pgm.height > pic_pgm.width) {
                 // Creates middle of image that gets increasingly dark towards the center
                 col_count = 0; // initialize
@@ -111,13 +119,13 @@ int main(int argc, char *argv[])  {
                     row_count = 0;
                     col_count++;
                     for(row = pic_pgm.height/4; row<pic_pgm.height/2; row++) {
-                        // Increases count proportionally with higher rows so it gets increasingly dark towards the bottom
+                        // Increases count proportionally with higher rows so it gets increasingly dark towards the right
                         if((col_count*slope)+(pic_pgm.height/4) > row) {
                             row_count++;
                         } 
-                        // Takes max grey value and subtracts by the greatest  number to get increasingly dark towards the right
+                        // Takes max grey value and subtracts by the proportional height to get increasingly dark towards the bottom
                         gray_level = (int)((float)maximum_gray_level-((float)row_count) * ((float)maximum_gray_level / (float)(pic_pgm.height/4)));
-                        // Mirror the gray value on each corner of the center
+                        // Mirror the gray value on each corner of the center portion
                         pic_pgm.image[row][col] = gray_level;
                         pic_pgm.image[(pic_pgm.height - 1) - row][col] = gray_level;
                         pic_pgm.image[row][(pic_pgm.width - 1) - col] = gray_level;
@@ -135,9 +143,9 @@ int main(int argc, char *argv[])  {
                         if((row_count/slope)+(pic_pgm.width/4) > col) {
                             col_count++;
                         } 
-                        // Takes max grey value and subtracts by the greatest  number to get increasingly dark towards the right
+                        // Takes max grey value and subtracts by the proportional width to get increasingly dark towards the right
                         gray_level = (int)((float)maximum_gray_level-((float)col_count) * ((float)maximum_gray_level / (float)(pic_pgm.width/4)));
-                        // Mirror the gray value on each corner of the center
+                        // Mirror the gray value on each corner of the center portion
                         pic_pgm.image[row][col] = gray_level;
                         pic_pgm.image[(pic_pgm.height - 1) - row][col] = gray_level;
                         pic_pgm.image[row][(pic_pgm.width - 1) - col] = gray_level;
@@ -153,31 +161,39 @@ int main(int argc, char *argv[])  {
         case PPM:
             // PPM image creation
             create_PPM_Image(&pic_ppm, image_width, image_height, maximum_gray_level); 
+            // proportion of colour for half the height of the image
             proportion = ((float)pic_ppm.height / 2.0) / (float)maximum_gray_level;
             row_count = 0; // initialize
-            for(row = 0; row<pic_ppm.height/2; row++) {            
+            // Loops for the top half of the image to fill in each colour gradient
+            for(row = 0; row<pic_ppm.height/2; row++) {
+                // Fills in red gradient
                 for(col = 0; col<pic_ppm.width/3; col++) {
                     pic_ppm.image[row][col][RED] = maximum_gray_level;
                     pic_ppm.image[row][col][GREEN] = (int)((float)row / proportion);
                     pic_ppm.image[row][col][BLUE] = (int)((float)row / proportion);
                 }
+                // Fills in green gradient
                 for(col = pic_ppm.width/3; col<(pic_ppm.width*2)/3; col++) {
                     pic_ppm.image[row][col][RED] = maximum_gray_level - (int)((float)row / proportion);
                     pic_ppm.image[row][col][GREEN] = maximum_gray_level;
                     pic_ppm.image[row][col][BLUE] = maximum_gray_level - (int)((float)row / proportion);
                 }
+                // Fills in blue gradient
                 for(col = (pic_ppm.width*2)/3; col<pic_ppm.width; col++) {
                     pic_ppm.image[row][col][RED] = (int)((float)row / proportion);
                     pic_ppm.image[row][col][GREEN] = (int)((float)row / proportion);
                     pic_ppm.image[row][col][BLUE] = maximum_gray_level;
                 }
             }
+            // Loops for the bottom half of the image to fill in both black and white gradients
             for(row = pic_ppm.height/2; row<pic_ppm.height; row++) {
+                // Gradient with black at top and white at bottom
                 for(col = 0; col<pic_ppm.width/2; col++) {
                     pic_ppm.image[row][col][RED] = (int)((float)row_count / proportion);
                     pic_ppm.image[row][col][GREEN] = (int)((float)row_count / proportion);
                     pic_ppm.image[row][col][BLUE] = (int)((float)row_count / proportion);
                 }
+                // Gradient with white at top and black at bottom
                 for(col = pic_ppm.width/2; col<pic_ppm.width; col++) {
                     pic_ppm.image[row][col][RED] = maximum_gray_level - (int)((float)row_count / proportion);
                     pic_ppm.image[row][col][GREEN] = maximum_gray_level - (int)((float)row_count / proportion);
@@ -190,7 +206,8 @@ int main(int argc, char *argv[])  {
             copy_PPM_to_PGM(&pic_ppm, &pic_pgm_green, GREEN);
             copy_PPM_to_PGM(&pic_ppm, &pic_pgm_blue, BLUE);
 
-            char * file_name_r = malloc(strlen(output_image) + 2 + 1); // 6 for extension plus identifier
+            // Create new file names for each colour grey map and save
+            char * file_name_r = malloc(strlen(output_image) + 2 + 1); // 2 extra for colour identifier
             char * file_name_g = malloc(strlen(output_image) + 2 + 1); 
             char * file_name_b = malloc(strlen(output_image) + 2 + 1); 
             strcpy(file_name_r, "R_");
@@ -204,6 +221,7 @@ int main(int argc, char *argv[])  {
             save_PGM_Image(&pic_pgm_green, file_name_g, image_format);
             save_PGM_Image(&pic_pgm_blue, file_name_b, image_format);
 
+            // Clean up memory
             free(file_name_r);
             free(file_name_g);
             free(file_name_b);
@@ -214,7 +232,7 @@ int main(int argc, char *argv[])  {
             break;
 
         default:
-            // Invalid number should be caught in initial size check
+            // Invalid image type should be caught in initial size check
             break;
     }
     return 0;

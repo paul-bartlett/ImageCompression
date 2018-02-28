8872a8fc4e1d53c7569f0e983dc4aa978f3fab42
paul-bartlett <pbartle7@uwo.ca>
2018-02-28 20:26:04 +0000

    Finished Q5 and starting Q6

--- Assignment2/huffman_encode_image.c
+++ Assignment2/huffman_encode_image.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include "libpnm.h"
 #include "generate_huffman_nodes.h"
-#include "huffman_encode_image.h"
 
 unsigned char *huffman_encode_image(struct PGM_Image *input_pgm_image, struct node *huffman_node, int number_of_nodes, unsigned long int *length_of_encoded_array) {
     
--- Assignment2/read_huffman_encoded_data.c
+++ Assignment2/read_huffman_encoded_data.c
@@ -1,42 +1,51 @@
 #include <stdio.h>
+#include "libpnm.h"
 #include "generate_huffman_nodes.h"
 
 unsigned char *read_huffman_encoded_data(char *compressed_file_name_ptr, int *image_width, int *image_height, int *max_gray_value, int *number_of_nodes, struct node **huffman_node, long int *length_of_encoded_image) {
 
     // For reading from file
     char c;
+    unsigned char *encoded_image; 
 
     // Open file for reading and check if valid
     FILE *imageFilePointer = fopen(compressed_file_name_ptr, "wb");
     if(imageFilePointer == NULL) {
         printf("Error opening file for reading\n");
-        return;
+        return 0;
     }
     
     // Make sure the first char is P
-    if(fgetc(imageFilePointer) != 'P')
-    { fclose(imageFilePointer);
+    if(fgetc(imageFilePointer) != 'P') { 
+        fclose(imageFilePointer);
         printf("Header error: no P\n");
         return 0;
     }
 
     // Make sure the second char is either a 2 or 5
     c = fgetc(imageFilePointer);
-    if(c != '2' && c != '5')
-    { fclose(imageFilePointer);
+    if(c != '2' && c != '5') { 
+        fclose(imageFilePointer);
         printf("Header error: no 2 or 5 for pgm\n"); 
         return 0;
     }
 
     // Get the width, height, max gray value, number of nodes 
-    image_width = geti(imageFilePointer);
-    image_height = geti(imageFilePointer);
-    max_gray_value = geti(imageFilePointer);
-    number_of_nodes = geti(imageFilePointer);
+    *image_width = geti(imageFilePointer);
+    *image_height = geti(imageFilePointer);
+    *max_gray_value = geti(imageFilePointer);
+    *number_of_nodes = geti(imageFilePointer);
 
     // Get the Huffman array values
-    
+    for(int i = 0; i < *number_of_nodes; i++) 
+        fscanf(imageFilePointer, "%d %d ", &huffman_node[i]->first_value, &huffman_node[i]->second_value);
 
-    // Get the length of encoded image, 
+    // Get the length of encoded image
+    fscanf(imageFilePointer, "%ld", length_of_encoded_image);
 
+    // Get the encoded image values
+    encoded_image = (unsigned char *)calloc(*length_of_encoded_image + 1, sizeof(char));
+    fscanf(imageFilePointer, "%s", encoded_image);
+
+    return encoded_image;
 }

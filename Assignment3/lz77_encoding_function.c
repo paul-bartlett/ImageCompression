b9c407251a62158ad8b6ce10965f3ce5305c190f
paul-bartlett <pbartle7@uwo.ca>
2018-03-08 17:39:39 +0000

    Q1 started

--- Assignment3/lz77_encoding_function.c
+++ Assignment3/lz77_encoding_function.c
@@ -1,5 +1,16 @@
 #include "lz77_encoding_function.h"
 
 void Encode_Using_LZ77(char *in_PGM_filename_Ptr, unsigned int searching_buffer_size,  float *avg_offset_Ptr, float *std_offset_Ptr, float *avg_length_Ptr, float *std_length_Ptr) {
+    
+    int offset, matching_length, next_symbol, current_buffer_size = 0;
+    struct PGM_Image pic_pgm;
+    load_PGM_Image(&pic_pgm, in_PGM_filename_Ptr);
 
+    for(int row = 0; row < pic_pgm.height; row++) {
+        for(int col = 0; col < pic_pgm.width; col++) {
+
+            for(int i = 0; i < searching_buffer_size && i < current_buffer_size; i++) {
+                
+            }
+    }
 }
--- Assignment3/lz77_encoding_function.h
+++ Assignment3/lz77_encoding_function.h
@@ -2,6 +2,7 @@
 #define LZ77_ENCODING_FUNCTION_H
 
 #include <stdio.h>
+#include "libpnm.h"
 
 void Encode_Using_LZ77(char *in_PGM_filename_Ptr, unsigned int searching_buffer_size,  float *avg_offset_Ptr, float *std_offset_Ptr, float *avg_length_Ptr, float *std_length_Ptr);
 

75351c02bba3a0b80c3fc736a066fd7453f23889
paul-bartlett <pbartle7@uwo.ca>
2018-03-29 12:52:37 +0000

    Initialize Assignment4

---
+++ Assignment4/mean_absolute_error.h
@@ -0,0 +1,10 @@
+#ifndef MEAN_ABSOLUTE_ERROR_H
+#define MEAN_ABSOLUTE_ERROR_H
+
+#include <stdio.h>
+#include <math.h>
+#include "libpnm.h"
+
+float mean_absolute_error(char *file_name_1_ptr, char *file_name_2_ptr);
+
+#endif // MEAN_ABSOLUTE_ERROR_H

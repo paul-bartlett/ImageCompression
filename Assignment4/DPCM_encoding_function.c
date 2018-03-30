462cfa1bccabee8a0ffe687b1fe2ae945b740a28
paul-bartlett <pbartle7@uwo.ca>
2018-03-30 12:58:41 +0000

    CALIC mode encoding complete, needs to write to file while processing to allow extra value for exiting binary mode

--- Assignment4/DPCM_encoding_function.c
+++ Assignment4/DPCM_encoding_function.c
@@ -41,8 +41,8 @@ void Encode_Using_DPCM(char *in_PGM_filename_Ptr, int prediction_rule, float *av
                     prediction_array[row][col] =  pic_pgm.image[row][col] - pic_pgm.image[row][col-1];
 
             break;
-        
-        // N prediction
+
+            // N prediction
         case 2:
             for(row = 2; row < height; row++)
                 for(col = 2; col < (width - 1); col++)
@@ -50,7 +50,7 @@ void Encode_Using_DPCM(char *in_PGM_filename_Ptr, int prediction_rule, float *av
 
             break;
 
-        // W/2 + N/2 prediction
+            // W/2 + N/2 prediction
         case 3:
             for(row = 2; row < height; row++)
                 for(col = 2; col < (width - 1); col++)
@@ -58,7 +58,7 @@ void Encode_Using_DPCM(char *in_PGM_filename_Ptr, int prediction_rule, float *av
 
             break;
 
-        // CALIC prediction
+            // CALIC prediction
         case 4:
             for(row = 2; row < height; row++) {
                 for(col = 2; col < (width - 1); col++) {
@@ -71,114 +71,128 @@ void Encode_Using_DPCM(char *in_PGM_filename_Ptr, int prediction_rule, float *av
                         nne = pic_pgm.image[row-2][col+1],
                         dh = abs(w - ww) + abs(n - nw) + abs(ne - n),
                         dv = abs(w - nw) + abs(n - nn) + abs(ne - nne),
-                        mode = 0,
+                        mode = 1,
                         mode_check[6] = { ww, n, nw, ne, nn, nne },
+                        mode_flag = 1,
                         binary = 1,
                         binary_check[2];
 
                         // Check if CALIC should be in binary mode or continuous mode
-                        if(mode == 0) {
+                        if(mode_flag) {
                             binary_check[0] = w;
+                            binary = 1;
                             for(int i = 0; i < 6; i++) {
+                                // Get both unique 
                                 if(binary != 2) {
                                     if(mode_check[i] != binary_check[0]) {
                                         binary_check[1] = mode_check[i];
                                         binary++;
                                     }
-                                } else {
-                                    if(binary_check[0] == mode_check[i] || binary_check[1] == mode_check[i]) {
+                                // Exit binary mode if >2 values found
+                                } else if(binary_check[0] != mode_check[i] && binary_check[1] != mode_check[i]) {
+                                    if(mode == 0) {
+                                        // fprintf(DPCM_file_pointer, "2 "); // Send 2 to exit binary mode
                                         mode = 1;
-                                        break;
                                     }
+                                    mode_flag = 0;
+                                    break;
                                 }
-                            }
-
-                            // CALIC binary mode
-                            if(mode == 1) {
-
-                            } 
-
-                            // CALIC continuous-tone mode
-                            if(mode == 1) {
-                                if(dv - dh > 80) 
-                                    prediction_array[row][col] = w;
-                                else if(dh - dv > 80) 
-                                    prediction_array[row][col] = n;
-                                else {
-                                    prediction_array[row][col] = (w + n)/2 + (ne - nw)/4;
-                                    if(dv - dh > 32)
-                                        prediction_array[row][col] = 1/2 * prediction_array[row][col] + 1/2 * w;
-                                    else if(dh - dv > 32)
-                                        prediction_array[row][col] = 1/2 * prediction_array[row][col] + 1/2 * n;
-                                    else if(dv - dh > 8)
-                                        prediction_array[row][col] = 3/4 * prediction_array[row][col] + 1/4 * w;
-                                    else if(dh - dv > 8)
-                                        prediction_array[row][col] = 3/4 * prediction_array[row][col] + 1/4 * n;
+                                // Start binary mode if loop is done
+                                if(i == 5) {
+                                    mode = 0;
                                 }
-
-                                prediction_array[row][col] =  pic_pgm.image[row][col] - prediction_array[row][col];
                             }
                         }
-                    }
-                }
-                break;
 
-                default:
-                printf("Incorrect prediction rule used, must be from 1-4\n");
-            }
+                        // CALIC binary mode
+                        if(mode == 0) {
+                            if(pic_pgm.image[row][col] == w)
+                                prediction_array[row][col] = 0;
+                            else 
+                                prediction_array[row][col] = 1;
+                        } 
 
-            // Write predicted values to a file
-            char DPCM_filename[strlen(in_PGM_filename_Ptr) + 2 + 5];
-            char errors_filename[strlen(in_PGM_filename_Ptr) + 2 + 7 + 4];
+                        // CALIC continuous-tone mode
+                        if(mode == 1) {
+                            if(dv - dh > 80) 
+                                prediction_array[row][col] = w;
+                            else if(dh - dv > 80) 
+                                prediction_array[row][col] = n;
+                            else {
+                                prediction_array[row][col] = (w + n)/2 + (ne - nw)/4;
+                                if(dv - dh > 32)
+                                    prediction_array[row][col] = 1/2 * prediction_array[row][col] + 1/2 * w;
+                                else if(dh - dv > 32)
+                                    prediction_array[row][col] = 1/2 * prediction_array[row][col] + 1/2 * n;
+                                else if(dv - dh > 8)
+                                    prediction_array[row][col] = 3/4 * prediction_array[row][col] + 1/4 * w;
+                                else if(dh - dv > 8)
+                                    prediction_array[row][col] = 3/4 * prediction_array[row][col] + 1/4 * n;
+                            }
 
-            sprintf(DPCM_filename, "%s.%d.DPCM", in_PGM_filename_Ptr, prediction_rule);
-            sprintf(errors_filename, "%s.%d.errors.csv", in_PGM_filename_Ptr, prediction_rule);
-
-            FILE *DPCM_file_pointer = fopen(DPCM_filename, "wb");
-            if(DPCM_file_pointer == NULL) printf("Error opening DPCM file for writing\n");
-
-            FILE *errors_file_pointer = fopen(errors_filename, "wb");
-            if(errors_file_pointer == NULL) printf("Error opening errors file for writing\n");
-
-            // Write the DPCM header
-            fprintf(DPCM_file_pointer, "P2\n%d %d\n%d\n%d\n", width, height, max_gray_value, prediction_rule);
-
-            // Write the prediction values and get frequency and sum
-            for(row = 0; row < height; row++) {
-                for(col = 0; col < width; col++) {
-                    fprintf(DPCM_file_pointer, "%d ", prediction_array[row][col]);
-                    error_array[abs(prediction_array[row][col])]++; // frequency
-                    error_sum += abs(prediction_array[row][col]); // for mean and stdev
-                }
-            }
-
-            // Write the error values and get sum for CSV and calculations
-            for(int i = 0; i < max_gray_value+1; i++)
-                if(error_array[i] != 0)
-                    fprintf(errors_file_pointer, "%d,%d\n", i, error_array[i]);
-
-            fclose(DPCM_file_pointer);
-            fclose(errors_file_pointer);
-
-            // Calculate the average and standard deviation of the errors
-            float error_mean, error_std = 0.0;
-
-            error_mean = (float) error_sum / (height * width);
-
-            for(row = 0; row < height; row++) {
-                for(col = 0; col < width; col++) {
-                    error_std += pow(abs(prediction_array[row][col]) - error_mean, 2);
+                            prediction_array[row][col] =  pic_pgm.image[row][col] - prediction_array[row][col];
+                        }
                 }
             }
 
-            error_std = sqrt(error_std / (height * width));
+            break;
 
-            // Save values in function parameters
-            *avg_abs_error_Ptr = error_mean;
-            *std_abs_error_Ptr = error_std;
-
-            // Free memory
-            free_PGM_Image(&pic_pgm);
-            //free(error_array);
+        default:
+            printf("Incorrect prediction rule used, must be from 1-4\n");
     }
+
+    // Write predicted values to a file
+    char DPCM_filename[strlen(in_PGM_filename_Ptr) + 2 + 5];
+    char errors_filename[strlen(in_PGM_filename_Ptr) + 2 + 7 + 4];
+
+    sprintf(DPCM_filename, "%s.%d.DPCM", in_PGM_filename_Ptr, prediction_rule);
+    sprintf(errors_filename, "%s.%d.errors.csv", in_PGM_filename_Ptr, prediction_rule);
+
+    FILE *DPCM_file_pointer = fopen(DPCM_filename, "wb");
+    if(DPCM_file_pointer == NULL) printf("Error opening DPCM file for writing\n");
+
+    FILE *errors_file_pointer = fopen(errors_filename, "wb");
+    if(errors_file_pointer == NULL) printf("Error opening errors file for writing\n");
+
+    // Write the DPCM header
+    fprintf(DPCM_file_pointer, "P2\n%d %d\n%d\n%d\n", width, height, max_gray_value, prediction_rule);
+
+    // Write the prediction values and get frequency and sum
+    for(row = 0; row < height; row++) {
+        for(col = 0; col < width; col++) {
+            fprintf(DPCM_file_pointer, "%d ", prediction_array[row][col]);
+            error_array[abs(prediction_array[row][col])]++; // frequency
+            error_sum += abs(prediction_array[row][col]); // for mean and stdev
+        }
+    }
+
+    // Write the error values and get sum for CSV and calculations
+    for(int i = 0; i < max_gray_value+1; i++)
+        if(error_array[i] != 0)
+            fprintf(errors_file_pointer, "%d,%d\n", i, error_array[i]);
+
+    fclose(DPCM_file_pointer);
+    fclose(errors_file_pointer);
+
+    // Calculate the average and standard deviation of the errors
+    float error_mean, error_std = 0.0;
+
+    error_mean = (float) error_sum / (height * width);
+
+    for(row = 0; row < height; row++) {
+        for(col = 0; col < width; col++) {
+            error_std += pow(abs(prediction_array[row][col]) - error_mean, 2);
+        }
+    }
+
+    error_std = sqrt(error_std / (height * width));
+
+    // Save values in function parameters
+    *avg_abs_error_Ptr = error_mean;
+    *std_abs_error_Ptr = error_std;
+
+    // Free memory
+    free_PGM_Image(&pic_pgm);
+    //free(error_array);
+}
 
